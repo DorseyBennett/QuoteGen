0f59339e4a6a163241bde277dd31128cbec37922
tappel2018 <tappel2018@francisparker.org>
2015-05-14 15:57:13 -0700

    ListFunctions.java
    Generic functions for arrays for randomization.

---
+++ ListFunctions.java
@@ -0,0 +1,29 @@
+package quoteGen;
+
+public class ListFunctions {
+	
+	public static int getRandomIndex(String[] list) {
+		int amount = list.length;
+
+		int randomPos = (int) (Math.random() * amount);
+
+		return randomPos;
+
+	}
+	
+	public static int getSimilarIndex(String[] list, int index)
+	{
+		
+		double offset = -list.length / 2;
+		
+		
+		for(int i = 0; i < list.length; i++)
+			offset += Math.random();
+		
+		index += (int) (offset + (Math.abs(offset)/offset));
+		
+		return (index + list.length)%list.length;
+		
+	}
+	
+}
