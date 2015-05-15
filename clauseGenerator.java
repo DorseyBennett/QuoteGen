5a66dfcc821ff2b74ec5e6db754d67287395676f
QuadFIsh <Ben@BenDorseys-MacBook-Pro.local>
2015-05-14 17:13:31 -0700

    Fixed bugs
    There would be 2 periods at the end of in the sentence.
    
    also spelled something wrong.
    
    forgot some return statements without adding Grammar

--- clauseGenerator.java
+++ clauseGenerator.java
@@ -25,33 +25,33 @@ public class clauseGenerator {
 		clause += getRandomString(PartsOfSpeech.verbs[verbType]) + "";
 
 		if (verbType == PartsOfSpeech.Prepositional) {
-			return Grammer(clause + " that " + getPrepositionalPhrase() + ", "
+			return Grammar(clause + " that " + getPrepositionalPhrase() + ", "
 					+ formClause());
 
 		}
 
 		if (verbType == PartsOfSpeech.StandAlone) {
 			if (Math.random() < 0.3)
-				return clause;
+				return Grammar(clause);
 			if (Math.random() < .7)
 				clause += " " + getRandomString(PartsOfSpeech.conjunctions)
 						+ " " + formClause();
-			return Grammer(clause + " " + getPrepositionalPhrase());
+			return Grammar(clause + " " + getPrepositionalPhrase());
 
 		}
 		if (verbType == PartsOfSpeech.DirectObject) {
 			clause += " " + getRandomString(PartsOfSpeech.places);
 			if (Math.random() < 0.3)
-				return Grammer(clause);
-			if (Math.random() < .7)
+				return Grammar(clause);
+			if (Math.random() < .7) 
 				clause += " " + getRandomString(PartsOfSpeech.conjunctions)
 						+ " " + getRandomString(PartsOfSpeech.places);
 			if (Math.random() < .4)
 				clause += " " + getRandomString(PartsOfSpeech.conjunctions)
 						+ " " + formClause();
 			if (Math.random() < 0.3)
-				return clause;
-			return Grammer(clause + " " + getPrepositionalPhrase());
+				return Grammar(clause);
+			return Grammar(clause + " " + getPrepositionalPhrase());
 		}
 
 		return "error";
@@ -68,11 +68,20 @@ public class clauseGenerator {
 		return phrase;
 	}
 
-	public static String Grammer(String toLowerCase) {
+	public static String Grammar(String toLowerCase) {
 		String end = toLowerCase.substring(1);
 		String begin = toLowerCase.substring(0, 1);
 		end = end.toLowerCase();
-		return begin + end + ".";
+		String all = begin+end;
+		if(all.indexOf(".")==-1)
+			return all + ".";
+		else if(all.indexOf(".")!=-1){
+			begin = all.substring(0,all.indexOf("."));
+			end = all.substring(all.indexOf(".")+1);
+			return begin+end+".";
+					
+		}
+		else return all;
 	}
 
 }
